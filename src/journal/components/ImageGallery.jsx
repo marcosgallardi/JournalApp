3472336fc28086fb6f908b0e2a3faea04c106069
Marcos Gallardi <gallardimarcos98@gmail.com>
2023-06-30 08:45:07 -0300

    commit completo

---
+++ src/journal/components/ImageGallery.jsx
@@ -0,0 +1,22 @@
+import { ImageListItem, ImageList } from '@mui/material';
+
+
+export const ImageGallery = ({ images }) => {
+
+  return (
+    <ImageList sx={{ width: '100%', height: 500 }} cols={4} rowHeight={200}>
+      { images?.map((images) => (
+        <ImageListItem key={images}>
+          <img
+            src={`${images}?w=164&h=164&fit=crop&auto=format`}
+            srcSet={`${images}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+            alt="Imagen de la nota"
+            loading="lazy"
+          />
+        </ImageListItem>
+      ))}
+    </ImageList>
+  );
+}
+
+
